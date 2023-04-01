0e7ea12a531ccfc8fd8f19e05c8aa2e3fc0606f0
Ravi <test_git_010@outlook.com>
2023-04-01 23:02:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const PORT = 3000;
+const SECRET_KEY = 'my_secret_key';
+
+// Middleware for parsing JSON data
+app.use(express.json());
+
+// In-memory database of documents
+const documents = [
+  { id: 1, title: 'Document 1' },
+  { id: 2, title: 'Document 2' },
+  { id: 3, title: 'Document 3' },
+];
+
+// Login API
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if the credentials are correct
+  if (username === 'admin' && password === 'admin') {
+    // Generate a JWT token with a 1 hour expiration time
+    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+
+    res.json({ token });
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+});
+
+// Details API with authentication middleware
+app.get('/documents', authenticateToken, (req, res) => {
+  res.json(documents);
+});
+
+// Middleware for authenticating the JWT token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.status(401).send('Missing token');
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).send('Invalid token');
+
+    req.user = user;
+    next();
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
